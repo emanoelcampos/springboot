5545a71eb631c69bb6efa6aa837acc5283bcf94e
emanoelcampos <emanoel.cv@gmail.com>
2023-10-17 22:43:59 -0300

    adicionando lita de series

---
+++ src/main/java/dev/emanoel/springboot/domain/Serie.java
@@ -0,0 +1,14 @@
+package dev.emanoel.springboot.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Serie {
+
+    private Long id;
+
+    private String name;
+
+}
